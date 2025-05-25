06e163d0d82e98bd4eaac323ed6daa4fb2ba9e43
KVaishnavRao <147793138+KVaishnavRao@users.noreply.github.com>
2025-05-25 13:45:49 +0530

    Add files via upload

---
+++ speech.py
@@ -0,0 +1,38 @@
+import speech_recognition as sr # type: ignore
+import nltk # type: ignore
+
+nltk.download('punkt')
+
+
+# Optional: Set custom nltk_data path
+nltk.data.path.append(r'C:\Users\K Vaishnav Rao\nltk_data')
+
+# Download tokenizer data
+nltk.download('punkt')
+
+def recognize_speech_from_mic():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Say something...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        text = recognizer.recognize_google(audio)
+        print("\nRecognized Text:")
+        print(text)
+
+        tokens = nltk.word_tokenize(text)
+        print("\nTokenized Words:")
+        print(tokens)
+
+        return text
+
+    except sr.UnknownValueError:
+        print("Could not understand the audio.")
+    except sr.RequestError as e:
+        print(f"Request failed; {e}")
+
+if __name__ == "__main__":
+    recognize_speech_from_mic()
